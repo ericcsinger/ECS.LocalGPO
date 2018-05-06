703cac4c043272874b38875b6d4e6d1c0a12177f
Eric C. Singer <ericcsinger@gmail.com>
2018-05-06 15:27:50 -0400

    Starting work in 1.1.0

---
+++ 1.1.0/ECS.LocalGPO.psm1
@@ -0,0 +1,5 @@
+﻿#Loading Local GPO Functions
+
+. "$PSScriptRoot\Functions\Get-ECSLocalGPOUserRightAssignment.ps1"
+. "$PSScriptRoot\Functions\Add-ECSLocalGPOUserRightAssignment.ps1"
+. "$PSScriptRoot\Functions\Remove-ECSLocalGPOUserRightAssignment.ps1"
